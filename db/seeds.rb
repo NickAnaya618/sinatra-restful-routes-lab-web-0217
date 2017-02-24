6e590f3dfa9fd9f7efdeec6171afb21b5abbee6e
NickAnaya618 <anaya.nicholas@gmail.com>
2017-02-23 19:42:34 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+recipe1 = Recipe.create(name: "sup", ingredients: "dawg", cook_time: 20)
+
+recipe2 = Recipe.create(name: "foo", ingredients: "bar", cook_time: 40)
+
+recipe3 = Recipe.create(name: "one", ingredients: "two", cook_time: 10)
